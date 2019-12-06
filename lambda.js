57800147e0ff4279363f677a3ce72382b56dbc7d
tangw197 <Curry.X.Tang.-ND@disney.com>
2019-12-06 11:45:42 +0800

    update

--- lambda.js
+++ lambda.js
@@ -1,5 +1,5 @@
 const awsLambdaFastify = require('./aws-lambda-fastify');
 const app = require('./app');
 
-const proxy = awsLambdaFastify(app);
+const proxy = awsLambdaFastify(app, { binaryMimeTypes: ['application/json']});
 exports.handler = (event, context, callback) => proxy(event, context, callback);
