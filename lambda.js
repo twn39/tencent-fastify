4bb5f529659ae8b5f8a438a86955d7293997a1ff
tangw197 <Curry.X.Tang.-ND@disney.com>
2019-12-06 11:20:18 +0800

    update

--- lambda.js
+++ lambda.js
@@ -2,4 +2,4 @@ const awsLambdaFastify = require('./aws-lambda-fastify');
 const app = require('./app');
 
 const proxy = awsLambdaFastify(app);
-exports.handler = (event, context) => proxy(event, context);
+exports.handler = proxy;
