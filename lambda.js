28c55233543867556db71317c6d8beb2f390482f
tangw197 <Curry.X.Tang.-ND@disney.com>
2019-12-06 10:44:47 +0800

    add lambda-fastify file

--- lambda.js
+++ lambda.js
@@ -1,4 +1,4 @@
-const awsLambdaFastify = require('aws-lambda-fastify');
+const awsLambdaFastify = require('./aws-lambda-fastify');
 const app = require('./app');
 
 const proxy = awsLambdaFastify(app);
