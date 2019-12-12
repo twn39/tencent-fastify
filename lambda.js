ee6e6fd1c05b5cc05d03cb747be9eca8ca2c1f3f
tangw197 <Curry.X.Tang.-ND@disney.com>
2019-12-12 13:10:29 +0800

    fix query string bug

--- lambda.js
+++ lambda.js
@@ -1,5 +1,5 @@
-const awsLambdaFastify = require('./aws-lambda-fastify');
+const tencentCloudFastify = require('./tencent-cloud-fastify');
 const app = require('./app');
 
-const proxy = awsLambdaFastify(app);
+const proxy = tencentCloudFastify(app);
 exports.handler = (event, context) => proxy(event, context);
---
+++ tencent-cloud-fastify.js
@@ -0,0 +1,79 @@
+module.exports = (app, options) => (event, context, callback) => {
+    options = options || {};
+    options.binaryMimeTypes = options.binaryMimeTypes || [];
+    if (options.callbackWaitsForEmptyEventLoop !== undefined) {
+        context.callbackWaitsForEmptyEventLoop =
+            options.callbackWaitsForEmptyEventLoop;
+    }
+    event.body = event.body || '';
+
+    const method = event.httpMethod;
+    const url = event.path;
+    const query = event.queryString || {};
+    const headers = Object.assign({}, event.headers);
+    const payload = Buffer.from(
+        event.body,
+        event.isBase64Encoded ? 'base64' : 'utf8',
+    );
+
+    delete event.body;
+    headers['x-apigateway-event'] = encodeURIComponent(JSON.stringify(event));
+    if (context)
+        headers['x-apigateway-context'] = encodeURIComponent(
+            JSON.stringify(context),
+        );
+
+    if (event.requestContext && event.requestContext.requestId) {
+        headers['x-request-id'] =
+            headers['x-request-id'] || event.requestContext.requestId;
+    }
+
+    const prom = new Promise((resolve, reject) => {
+        app.inject({ method, url, query, payload, headers }, (err, res) => {
+            if (err) {
+                console.error(err);
+                return resolve({
+                    statusCode: 500,
+                    body: '',
+                    headers: {},
+                });
+            }
+            // chunked transfer not currently supported by API Gateway
+            if (headers['transfer-encoding'] === 'chunked')
+                delete headers['transfer-encoding'];
+            if (headers['Transfer-Encoding'] === 'chunked')
+                delete headers['Transfer-Encoding'];
+
+            let multiValueHeaders;
+            Object.keys(res.headers).forEach(h => {
+                if (Array.isArray(res.headers[h])) {
+                    if (h.toLowerCase() === 'set-cookie') {
+                        multiValueHeaders = multiValueHeaders || {};
+                        multiValueHeaders[h] = res.headers[h];
+                        delete res.headers[h];
+                    } else res.headers[h] = res.headers[h].join(',');
+                }
+            });
+
+            const contentType = (
+                res.headers['content-type'] ||
+                res.headers['Content-Type'] ||
+                ''
+            ).split(';')[0];
+            const isBase64Encoded =
+                options.binaryMimeTypes.indexOf(contentType) > -1;
+
+            const ret = {
+                statusCode: res.statusCode,
+                body: isBase64Encoded
+                    ? res.rawPayload.toString('base64')
+                    : res.payload,
+                headers: res.headers,
+                isBase64Encoded,
+            };
+            resolve(ret);
+        });
+    });
+    if (!callback) return prom;
+    prom.then(ret => callback(null, ret)).catch(callback);
+};
